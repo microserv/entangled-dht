25e5bdc79b8bdca8eb7b70a92ca4728ee4cf565d
francois <francois@b64df090-e26a-408d-8e9f-faa0e23cf7b6>
2007-09-19 13:28:37 +0000

    Added a test for non-existent keys in a datastore
    git-svn-id: http://svn.code.sf.net/p/entangled/code@14 b64df090-e26a-408d-8e9f-faa0e23cf7b6

--- entangled/tests/testDatastore.py
+++ entangled/tests/testDatastore.py
@@ -12,20 +12,19 @@ class DataStoreTest(unittest.TestCase):
     """ Basic tests case for the reference DataStore API and implementation """
     def setUp(self):
         self.ds = kademlia.datastore.DataStore()
-    
-    def testReadWrite(self):
         h = hashlib.sha1()
         h.update('g')
         hashKey = h.digest()
-        cases  = (('a', 'hello there\nthis is a test'),
-                  ('b', unicode('jasdklfjklsdj;f2352352ljklzsdlkjkasf\ndsjklafsd')),
-                  ('e', 123),
-                  ('f', [('this', 'is', 1), {'complex': 'data entry'}]),
-                  ('aMuchLongerKeyThanAnyOfThePreviousOnes', 'some data'),
-                  (hashKey, 'some data'))
+        self.cases = (('a', 'hello there\nthis is a test'),
+                     ('b', unicode('jasdklfjklsdj;f2352352ljklzsdlkjkasf\ndsjklafsd')),
+                     ('e', 123),
+                     ('f', [('this', 'is', 1), {'complex': 'data entry'}]),
+                     ('aMuchLongerKeyThanAnyOfThePreviousOnes', 'some data'),
+                     (hashKey, 'some data'))
     
+    def testReadWrite(self):
         # Test write ability
-        for key, value in cases:
+        for key, value in self.cases:
             try:
                 self.ds[key] = value
             except Exception:
@@ -33,7 +32,7 @@ class DataStoreTest(unittest.TestCase):
                 self.fail('Failed writing the following data: key: "%s", data: "%s"\n  The error was: %s:' % (key, value, traceback.format_exc(5)))
             
         # Verify writing (test query ability)
-        for key, value in cases:
+        for key, value in self.cases:
             try:
                 self.failUnless(key in self.ds, 'Key "%s" not found in DataStore!' % key)
             except Exception:
@@ -41,8 +40,12 @@ class DataStoreTest(unittest.TestCase):
                 self.fail('Failed verifying that the following key exists: "%s"\n  The error was: %s:' % (key, traceback.format_exc(5)))
         
         # Read back the data
-        for key, value in cases:
+        for key, value in self.cases:
             self.failUnlessEqual(self.ds[key], value, 'DataStore returned invalid data! Expected "%s", got "%s"' % (value, self.ds[key]))
+    
+    def testNonExistentKeys(self):
+        for key, value in self.cases:
+            self.failIf(key in self.ds, 'DataStore reports it has non-existent key: "%s"') 
 
 def suite():
     suite = unittest.TestSuite()
