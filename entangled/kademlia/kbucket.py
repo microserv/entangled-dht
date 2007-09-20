d2ccaef8511763b075a8132527c57e606138e93a
neil <neil@b64df090-e26a-408d-8e9f-faa0e23cf7b6>
2007-09-20 06:53:35 +0000

    - Finished addContact() and getContacts() implementation. - Added removeContact() for a KBucket implementation that does not use RPC_PING. - Finished test script - testKBucket.py
    git-svn-id: http://svn.code.sf.net/p/entangled/code@17 b64df090-e26a-408d-8e9f-faa0e23cf7b6

--- entangled/kademlia/kbucket.py
+++ entangled/kademlia/kbucket.py
@@ -7,37 +7,92 @@
 
 import kademlia.constants
 
+# remove when protocol in place
+pingResult = False
+
 class KBucket:
     def __init__(self):
-        self._contacts = dict()
+        self._contacts = list()
 
     def addContact(self, contact):
-        """ Add contact to _contact dictionary in the right order.
+        """ Add contact to _contact list in the right order.
+
+        @note: Will need to fix up when protocol up and running
 
         @return: True if contact added otherwise return false 
         @rtype: bool
         """
+
         # check to see if there is space to add new contact
-        # if there is - add to the bottom of dict
-        currentLength = len(self._contacts)
-        if currentLength < kademlia.constants.k:
-            self._contacts[currentLength+1] = contact
+        # if there is - add to the bottom
+        if len(self._contacts) < kademlia.constants.k:
+            self._contacts.append(contact)
         else:
             # check to see if the first contact in dict is still alive
-            result = rpc_ping(self._contacts[1])
+            # replace this function when needed
+            result = rpc_ping(self._contacts[0])
 
             if not result:
-                position = 2
-                while position <= kademlia.constants.k:
-                    self._contacts[position-1] = self._contact[position]
-                    position += 1
-
-                self._contacts[kademlia.constants.k] = contact
+                # first contact did not respond - remove and add new contact
+                self._contacts.remove(self._contacts[0])
+                self._contacts.append(contact)
             else:
+                # first contact responded, now add to bottom
+                tmpContact = self._contacts[0]
+                self._contacts.remove(tmpContact)
+                self._contacts.append(tmpContact)
+                
+                # No space
                 return False
 
+        # everything went smoothly
         return True
 
     def getContacts(self, count):
-        pass
+        """ Returns a list containing up to the first count number of contacts
+        
+        @return: Return up to the first count number of contacts in a list/tuple
+                If no contacts are present a null-list is returned
+        @rtype: list
+        """
+        # Get current contact number
+        currentLen = len(self._contacts)
+
+        # If count greater than k - return only k contacts
+        # !!VERIFY!! behaviour
+        if count > kademlia.constants.k:
+            count = kademlia.constants.k
+
+        # Check if count value in range and,
+        # if count number of contacts are available
+        if not currentLen:
+            contactList = list()
+        # length of list less than requested amount
+        elif currentLen < count:
+            contactList = self._contacts[0:currentLen]
+        # enough contacts in list
+        else:
+            contactList = self._contacts[0:count]
+
+        return contactList
+
+    def removeContact(self, index):
+        """ Remove given contact from list
+        
+        @return: Return true if operation successful else return false
+        @rtype: bool
+        """
+
+        # Check to see if index is valid
+        if index > kademlia.constants.k-1 or index > len(self._contacts)-1:
+            return False
+        # Remove contact
+        else:
+            tmpContact = self._contacts[index]
+            self._contacts.remove(tmpContact)
+            return True
+
+
+def rpc_ping(contact):
+    return pingResult
 
--- entangled/tests/testKBucket.py
+++ entangled/tests/testKBucket.py
@@ -5,6 +5,8 @@
 import unittest
 
 import kademlia.kbucket
+import kademlia.contact as contact
+import kademlia.constants
 
 class KBucketTest(unittest.TestCase):
     """ Test case for the KBucket class """
@@ -12,10 +14,81 @@ class KBucketTest(unittest.TestCase):
         self.kbucket = kademlia.kbucket.KBucket()
 
     def testAddContact(self):
-        pass
+        # test if contacts can be added to empty list
+        # add k contacts to bucket
+        for i in range(kademlia.constants.k):
+            tmpContact = contact.Contact(i,i,i,i)
+            result = self.kbucket.addContact(tmpContact)
+            self.failIf(result != True, "Could not add contact to non-full bucket")
+            self.failIf(self.kbucket._contacts[i] != tmpContact, "Contact in position %s not then same as the newly added contact" % (str(i)))
+
+        # test if contact can be added when the first contact didn't respond
+        kademlia.kbucket.pingResult = False
+        i += 1
+        tmpContact = contact.Contact(i,i,i,i)
+        result = self.kbucket.addContact(tmpContact)
+        self.failIf(result != True, "Could not add contact to end of bucket")
+        self.failIf(self.kbucket._contacts[i-1] != tmpContact, "Contact in position %s not then same as the newly added contact" % (str(i)))
+
+        # test if contact is not added to full list when first contact responds
+        kademlia.kbucket.pingResult = True
+        i += 1
+        tmpContact = contact.Contact(i,i,i,i)
+        result = self.kbucket.addContact(tmpContact)
+        self.failIf(result != False, "Contact should not have been added to the bucket")
 
     def testGetContacts(self):
-        pass
+        # clean contact bucket
+        self.kbucket._contacts = list()
+
+        # try and get 2 contacts from empty list
+        result = self.kbucket.getContacts(2)
+        self.failIf(len(result) != 0, "Returned list should be empty - list length %s" % (len(result)))
+
+        # Add k-2 contacts
+        for i in range(kademlia.constants.k-2):
+            tmpContact = contact.Contact(i,i,i,i)
+            result = self.kbucket.addContact(tmpContact)
+            self.failIf(result != True, "Could not add contact to non-full bucket")
+
+        # try to get too many contacts
+        # requested count greater than bucket size
+        result = self.kbucket.getContacts(kademlia.constants.k + 3)
+        self.failIf(len(result) != kademlia.constants.k-2, "Too many contacts in returned list %s - should be %s" % (len(result), kademlia.constants.k-2))
+
+        # verify returned contacts in list
+        for i in range(kademlia.constants.k-2):
+            self.failIf(self.kbucket._contacts[i].id != i, "Contact in position %s not same as added contact" % (str(i)))
+        
+        # try to get too many contacts
+        # requested count one greater than number of contacts
+        result = self.kbucket.getContacts(kademlia.constants.k-1)
+        self.failIf(len(result) != kademlia.constants.k-2, "Too many contacts in returned list %s - should be %s" % (len(result), kademlia.constants.k-2))
+
+        # try to get contacts
+        # requested count less than contact number
+        result = self.kbucket.getContacts(kademlia.constants.k-3)
+        self.failIf(len(result) != kademlia.constants.k-3, "Too many contacts in returned list %s - should be %s" % (len(result), kademlia.constants.k-3))
+
+
+    def testRemoveContact(self):
+        # clean contact bucket
+        self.kbucket._contacts = list()
+
+        # try remove contact from empty list
+        result = self.kbucket.removeContact(1)
+        self.failIf(result != False, "Returned result should be false - contact not in bucket. Bucket empty")
+
+        # Add couple contacts
+        for i in range(kademlia.constants.k-2):
+            tmpContact = contact.Contact(i,i,i,i)
+            result = self.kbucket.addContact(tmpContact)
+            self.failIf(result != True, "Could not add contact to non-full bucket")
+
+        # try remove contact from empty list
+        result = self.kbucket.removeContact(1)
+        self.failIf(result != True, "Could not remove contact from bucket")
+
 
 def suite():
     suite = unittest.TestSuite()
