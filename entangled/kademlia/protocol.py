06fca19d3dfa677237869c6a9158894cd556ab49
francois <francois@b64df090-e26a-408d-8e9f-faa0e23cf7b6>
2007-09-20 14:20:33 +0000

    Initial Twisted code for the Protocol class
    git-svn-id: http://svn.code.sf.net/p/entangled/code@21 b64df090-e26a-408d-8e9f-faa0e23cf7b6

--- entangled/kademlia/protocol.py
+++ entangled/kademlia/protocol.py
@@ -5,9 +5,10 @@
 # The docstrings in this module contain epytext markup; API documentation
 # may be created by processing this file with epydoc: http://epydoc.sf.net
 
-from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
 
-class KademliaProtocol(Protocol):
+class KademliaProtocol(DatagramProtocol):
     """ Implements all network-related functions of a Kademlia node """
     def __init__(self):
         pass
@@ -15,7 +16,28 @@ class KademliaProtocol(Protocol):
     def ping(self, contact):
         """ Sends a PING request to the specified contact
         
+        @todo: This function will change to use a proper RPC PING
+               as soon as we've done the RPC stuff.
+        
         @param contact: The recipient of the RPC
         @type contact: kademlia.contact.Contact
         """
-        pass
+        self._sendMessage(contact, 'PING')
+
+
+    def _sendMessage(self, contact, message):
+        """ Send an RPC message via UDP 
+        
+        @todo: This function will change significantly as soon as
+               the higher-level RPC stuff is sorted out. For instance,
+               it needs to track RPC identification numbers, response
+               timeouts, etc.
+        
+        @param contact: The recipient Kademlia node
+        @type contact: kademlia.contact.Contact
+        
+        @param message: The message to send
+        @type message: str
+        """
+        self.transport.connect(contact.address, contact.port)
+        self.transport.write(datagram)
