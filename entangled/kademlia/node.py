149efc61f014061193eb9c9ad7ea1c27525d6ed7
francois <francois@b64df090-e26a-408d-8e9f-faa0e23cf7b6>
2007-09-26 07:36:35 +0000

    Added the "rpcmethod" decorator for publishing Node methods as RPC methods Implemented "findNode" Started implementing some more RPC methods, including an iterative node lookup algorithm
    git-svn-id: http://svn.code.sf.net/p/entangled/code@33 b64df090-e26a-408d-8e9f-faa0e23cf7b6

--- entangled/kademlia/node.py
+++ entangled/kademlia/node.py
@@ -9,38 +9,107 @@
 
 import hashlib, random
 
-import datastore
+import constants
 import kbucket
+import datastore
+import network
 
+def rpcmethod(func):
+    """ Decorator to expose methods as RPC calls """
+    func.rpcmethod = True
+    return func
 
-class Node:
-    def __init__(self, knownNodes=None, dataStore=datastore.DataStore()):
+class Node(object):
+    def __init__(self, knownNodes=None, dataStore=None, networkPresence=None):
         self.id = self._generateID()
-        self._buckets = [] # remove?
-        self._connection = None
-        self._dataStore = dataStore
-        self._kbucket = kbucket.KBucket()
+        # Create k-buckets (for storing contacts)
+        self._buckets = []
+        for i in range(160):
+            self._buckets.append(kbucket.KBucket())
+        # Initialize this node's network access mechanisms
+        if networkPresence == None:
+            self._presence = network.NodePresence(self)
+        else:
+            self._presence = networkPresence
+        # Initialize the data storage mechanism used by this node
+        if dataStore == None:
+            self._dataStore = datastore.DataStore()
+        else:
+            self._dataStore = dataStore
 
-    def _generateID(self):
-        """ Generates a 160-bit pseudo-random identifier
+    def _iterativeFindNode(self, key):
+        """ The basic Kademlia node lookup operation
         
-        @return: A globally unique 160-bit pseudo-random identifier
-        @rtype: str
+        This builds a list of k "closest" contacts through iterative use of
+        the "FIND_NODE" RPC """
+        shortlist = self.findNode(key, constants.alpha)
+        # Note the closest known node
+        #TODO: possible IndexError exception here:
+        closestNode = shortlist[0]
+        for contact in shortlist:
+            if self._distance(key, contact.id) < self._distance(key, closestNode.id):
+                closestNode = contact
+    
+        def extendShortlist(rpcContacts):
+            for contact in rpcContacts:
+                if contact not in shortlist:
+                    shortlist.append(contact)
+        
+        def removeFromShortlist(errorContact):
+            if errorContact in shortlist:
+                shortlist.remove(errorContact)
+        
+        # Send parallel, asynchronous FIND_NODE RPCs to the shortlist of contacts
+        alreadyContacted = []
+        for contact in shortlist:
+            #TODO: add deferred, timeouts - remove from shortlist if timeout
+            df = contact.findNode(key)
+            df.addErrback(removeFromShortlist)
+            df.addCallback(extendShortlist)
+            alreadyContacted.append(contact)
+                
+
+    
+    def findNode(self, key, count=constants.k):
+        """ Finds C{k} known nodes closest to the node/value with the
+        specified key.
+        
+        @param key: the 160-bit key (i.e. the node or value ID) to search for
+        @type key: str
+        @param count: the amount of contacts to return (this defaults to C{k})
+        @type count: int
+        
+        @return: A list of node contacts (C{kademlia.contact.Contact instances})
+                 closest to the specified key. 
+                 This method will return C{k} (or C{count}, if specified)
+                 contacts if at all possible; it will only return fewer if the
+                 node is returning all of the contacts that it knows of.
+        @rtype: list
         """
-        hash = hashlib.sha1()
-        hash.update(str(random.getrandbits(255)))  
-        return hash.digest()
+        distance = self._distance(self.id, contact.id)
+        bucketIndex = int(math.log(distance, 2))
+        closestNodes = self._buckets[bucketIndex].getContacts(constants.k)
+        # The node must return k contacts, unless it does not know at least 8
+        i = 1
+        canGoLower = bucketIndex-i >= 0
+        canGoHigher = bucketIndex+i < 160
+        # Fill up the node list to k nodes, starting with the closest neighbouring nodes known 
+        while len(closestNodes) < constants.k and (canGoLower or canGoHigher):
+            #TODO: this needs to be optimized
+            if canGoLower:
+                closestNodes.extend(self._buckets[bucketIndex-i].getContacts(constants.k - len(closestNodes)))
+                canGoLower = bucketIndex-(i+1) >= 0
+            if canGoHigher:
+                closestNodes.extend(self._buckets[bucketIndex+i].getContacts(constants.k - len(closestNodes)))
+                canGoHigher = bucketIndex+(i+1) < 160
+            i += 1
+        return closestNodes
+        
 
-    def _lookupNode(self, key):
-        pass
-    
-    def findNode(self, key):
-        pass
-    
-    def updateContact(self, contact):
-        """ Update the given contact
+    def addContact(self, contact):
+        """ Add/update the given contact
 
-        @param contacts: kademlia.contact.Contact 
+        @param contact: kademlia.contact.Contact 
         @note: It is assumed that the bucket is -
             1) the contact is alive - timeout stuff sorted before method called!
             2) not full
@@ -51,9 +120,25 @@ class Node:
         """
         try:
             self._kbucket.addContact(contact)
-        except self._kbucket.BucketFull, e:
-            print "Warning: " + e.message()
+        except kbucket.BucketFull, e:
+            print 'addContact(): Warning: ', e
             updateContacts(contact)
+
+    def removeContact(self, contact):
+        """ Remove the specified contact from this node's table of known nodes
+        
+        @param contact: The contact to remove
+        @type contact: kademlia.contact.Contact
+        
+        @raise ValueError: Raised if the contact isn't found
+        """
+        distance = self._distance(self.id, contact.id)
+        bucketIndex = int(math.log(distance, 2))
+        try:
+            self._buckets[bucketIndex].remove(contact)
+        except ValueError:
+            print 'removeContact(): Warning: ', e
+            raise
 
     def updateContacts(self, contact):
         """ Update all current contacts in bucket by sending a ping request to all of them
@@ -66,17 +151,7 @@ class Node:
         
         # COMPARE RTT (round trip time)
 
-
-    def _distance(self, keyOne, keyTwo):
-        """ Calculate the XOR result between two string variables
-        
-        @return: XOR result of two long variables
-        @rtype: long
-        """
-        valKeyOne = long(keyOne.encode('hex'), 16)
-        valKeyTwo = long(keyTwo.encode('hex'), 16)
-        return valKeyOne ^ valKeyTwo
-
+    @rpcmethod
     def store(self, key, value):
         """ Store the received data in the local hash table
         
@@ -88,7 +163,8 @@ class Node:
                -Francois
         """
         self._dataStore[key] = value
-        
+    
+    @rpcmethod
     def findValue(self, key):
         """ Return the value associated with the specified key if present in
         this node's data, otherwise execute FIND_NODE for the key
@@ -108,3 +184,27 @@ class Node:
         else:
             return self.findNode(key)
         
+    def ping(self):
+        """ Used to verify contact between two Kademlia nodes """
+        return 'pong'
+
+    def _generateID(self):
+        """ Generates a 160-bit pseudo-random identifier
+        
+        @return: A globally unique 160-bit pseudo-random identifier
+        @rtype: str
+        """
+        hash = hashlib.sha1()
+        hash.update(str(random.getrandbits(255)))  
+        return hash.digest()
+
+    def _distance(self, keyOne, keyTwo):
+        """ Calculate the XOR result between two string variables
+        
+        @return: XOR result of two long variables
+        @rtype: long
+        """
+        valKeyOne = long(keyOne.encode('hex'), 16)
+        valKeyTwo = long(keyTwo.encode('hex'), 16)
+        return valKeyOne ^ valKeyTwo
+
