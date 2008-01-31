dbfcbae0db45e74c3c06a2a96383e8c5d94d95c7
boknesmonster <boknesmonster@b64df090-e26a-408d-8e9f-faa0e23cf7b6>
2008-01-31 16:03:17 +0000

    Added some package documentation Exposed some of the main classes through the packages themselves
    git-svn-id: http://svn.code.sf.net/p/entangled/code@152 b64df090-e26a-408d-8e9f-faa0e23cf7b6

--- entangled/entangled/__init__.py
+++ entangled/entangled/__init__.py
@@ -1,3 +1,24 @@
 # This library is free software, distributed under the terms of
 # the GNU Lesser General Public License Version 3, or any later version.
 # See the COPYING file included in this archive
+
+""" Entangled DHT and distributed tuple space
+
+The distributed hash table (DHT) used by Entangled is based on Kademlia,
+and be accessed by the "entangled.kademlia package", or by simply
+instantiating/subclassing the exposed KademliaNode in the main "entangled"
+package.
+
+On top of this Kademlia node Entangled provides some extra functionality
+in the form of a "DELETE" RPC and keyword-based search operations; these
+functions are accessible via the EntangledNode class in the main "entangled"
+package.
+
+The Entangled distributed tuple space is exposed as the
+DistributedTupleSpacePeer class, accessible via the main "entangled" package
+or its "dtuple" module.
+"""
+
+from kademlia.node import Node as KademliaNode
+from node import EntangledNode
+from dtuple import DistributedTupleSpacePeer
--- entangled/entangled/kademlia/__init__.py
+++ entangled/entangled/kademlia/__init__.py
@@ -1,3 +1,30 @@
 # This library is free software, distributed under the terms of
 # the GNU Lesser General Public License Version 3, or any later version.
 # See the COPYING file included in this archive
+
+""" Kademlia DHT implementation
+
+This package contains Entangled's implementation of the Kademlia
+distributed hash table (DHT).
+
+The main modules in this package are "node" (which contains the Kademlia
+implementation's main interface, namely the Node class), "datastore"
+(physical data storage mechanisms), "constants" (several constant values
+defining the Kademlia network), "routingtable" (different Kademlia routing
+table implementations) and "protocol" (actual network communications).
+
+The Node class is directly exposed in the main Entangled package ("entangled")
+as KademliaNode, and as Node in this package ("entangled.kademlia"). It is
+designed to be customizable; the data storage mechansims may (and should) be
+directly specified by client applications via the node's contructor. The same
+holds true for the node's routing table and network protocol used. This
+potentially allows the Kademlia node to be used with a TCP-based protocol,
+instead of the provided UDP-based one.
+
+Client applications should also modify the values found in
+entangled.kademlia.constants to suit their needs. Refer to the constants
+module for documentation on what these values control. 
+"""
+
+from entangled.kademlia.node import Node
+from entangled.kademlia.datastore import DictDataStore, SQLiteDataStore
